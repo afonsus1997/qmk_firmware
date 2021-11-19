ecbb35b5c9d02997e8522c668c618a98906d9927
Afonso Muralha <afonsomuralha@gmail.com>
2021-11-19 01:07:42 +0000

    fixed folders and disabled rgb

--- keyboards/kprepublic/bm68hsrgb/keymaps/afonsus1997/rules.mk
+++ keyboards/kprepublic/bm68hsrgb/keymaps/afonsus1997/rules.mk
@@ -19,9 +19,11 @@ NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
+
+UNICODE_ENABLE = yes
 
 LAYOUTS = 65_ansi
 
